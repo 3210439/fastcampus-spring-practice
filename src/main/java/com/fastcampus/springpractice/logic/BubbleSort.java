0e5c298770bc7065e1030015cb4e47f620febc4a
Godric <kimdeiz@naver.com>
2021-12-05 12:04:20 +0900

    finished this project

--- src/main/java/com/fastcampus/springpractice/logic/BubbleSort.java
+++ src/main/java/com/fastcampus/springpractice/logic/BubbleSort.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-// component 어노테이션 덕분에 빈으로 인식된다.
 @Component
 public class BubbleSort<T extends Comparable<T>> implements Sort<T>{
 
